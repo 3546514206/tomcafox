476f13d5f27f1471be3f689e5226cef845270096
杨海波 <3546514206@qq.com>
2022-11-21 18:03:52 +0800

    重新格式化

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <boost/config.hpp>
 
 
-#include "./cinatra/cinatra.hpp"
+#include "./tomfox/tomfox.hpp"
 
-
+// 测试 boost 是否安装成功
 void test_boost() {
     std::cout << BOOST_VERSION << std::endl;        //	boost版本号
     std::cout << BOOST_LIB_VERSION << std::endl;    //	boost版本号
@@ -15,27 +15,22 @@ void test_boost() {
     std::cout << BOOST_STDLIB << std::endl;          //	标准库
 }
 
-void start_server() {
+void start_test_server() {
     int max_thread_num = std::thread::hardware_concurrency();
     tomfox::http_server server(max_thread_num);
     server.listen("0.0.0.0", "8017");
     server.set_http_handler<tomfox::GET, tomfox::POST>("/http",
                                                        [](tomfox::request &req, tomfox::response &res) {
-                                                             char hello[] = "hello setsuna,i am tomfox!";
-                                                             res.set_status_and_content(tomfox::status_type::ok,
-                                                                                        hello);
-                                                         });
-
-    server.set_http_handler<tomfox::GET, tomfox::POST>("/char", [](tomfox::request &req, tomfox::response &res) {
-        char str[] = "hello,setsuna!";
-        res.set_status_and_content(tomfox::status_type::ok, str);
-    });
+                                                           char hello[] = "hello,i am tomfox!";
+                                                           res.set_status_and_content(tomfox::status_type::ok,
+                                                                                      hello);
+                                                       });
 
     server.run();
 }
 
 int main() {
     test_boost();
-    start_server();
+    start_test_server();
     return 0;
 }
